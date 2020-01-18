4354340db9e31335bb17d915a08f13bbd0a7626e
Bartek <cetlinskib@gmail.com>
2020-01-18 22:32:04 +0100

    Ester egg

--- application/database.py
+++ application/database.py
@@ -5,11 +5,15 @@ class DBProvider:
     def __init__(self):
         self.applications = db.AppDatabase.Apps
 
-    def getApplicationsDictionaty(self):
+    def getApplicationsDictionary(self):
         apps = self.applications.find()
         app_dict = []
         for a in apps:
             del a['_id']
+            try:
+                del a['computationStepsPackage']
+            except Exception:
+                print("nothing interestiong here")#Fuck it
             app = Application(a)
             app_dict.append(app.__repr__())
         return app_dict
@@ -17,3 +21,6 @@ class DBProvider:
     def saveApplicationDictionary(self,app):
         self.applications.insert_one(app)
 
+    def getApplicationCSP(self, app_id):
+        app = self.applications.find_one({"id":app_id})
+        return app['computationStepsPackage']
--- application/library.py
+++ application/library.py
@@ -7,38 +7,128 @@ db_worker = DBProvider()
 
 dummySchema = [
                         {
+                            "name": "N",
+                            "type": "int",
+                            "defaultValue": "10"
+                        }
+    ]
+
+schemaMoreDummyThanDummy = [
+
+]
+
+otherSchema = [
+                        {
                             "name": "Variable X",
                             "type": "int",
-                            "defaultValue": 1
+                            "defaultValue": "1"
                         },
-
                         {
                             "name": "Variable Y",
                             "type": "int",
-                            "defaultValue": 2
+                            "defaultValue": "2"
                         },
-
                         {
                             "name": "Variable Z",
                             "type": "int",
-                            "defaultValue": 3
+                            "defaultValue": "3"
                         }
-    ]
+]
+
+computation_step_package_fibonacci = {
+        "applicationId": "",
+        "computationSteps":[
+            {
+                "artifactUrl": "fib_list:latest",
+                "command": "",
+                "params": [
+                    {
+                        "name": "N",
+                        "type": "int",
+                        "defaultValue": 10
+                    }
+                ]
+            },
+            {
+                "params": [],
+                "artifactUrl": "fib_sum:latest",
+                "command": ""
+            }
+        ],
+        "version":"0.01.1"
+    }
+
+computation_step_package_cyberpunk = {
+        "applicationId": "",
+        "computationSteps":[
+            {
+                "artifactUrl": "cyberpunk2077.exe",
+                "command": "Run Cyberpunk2077",
+                "params": []
+            }
+        ],
+        "version":"2020.09.17"
+    }
+
+computation_step_package_default = {
+        "applicationId": "",
+        "computationSteps":[
+            {
+                "artifactUrl": "default",
+                "command": "default",
+                "params": [
+                        {
+                            "name": "Variable X",
+                            "type": "int",
+                            "defaultValue": "1"
+                        }
+                ]
+            },
+            {
+                "artifactUrl": "default",
+                "command": "default",
+                "params": [
+                        {
+                            "name": "Variable Y",
+                            "type": "int",
+                            "defaultValue": "2"
+                        }
+                ]
+            },
+            {
+                "artifactUrl": "default",
+                "command": "default",
+                "params": [
+                        {
+                            "name": "Variable Z",
+                            "type": "int",
+                            "defaultValue": "3"
+                        }
+                ]
+            }
+        ],
+        "version":"2020.07.07"
+    }
 
 @app.route('/library/launcher/applications', methods={'POST','GET'})
 def get_applications():
     if request.method == 'GET':
-        return json.dumps(db_worker.getApplicationsDictionaty())
+        return json.dumps(db_worker.getApplicationsDictionary())
     elif request.method == 'POST':
         form_data = request.form
+        print(dummySchema if 'fibonacci' in form_data else (schemaMoreDummyThanDummy if 'nothing' in form_data else otherSchema))
         app = {
-            'id':db_worker.getApplicationsDictionaty().__len__(),
-            'name':form_data['name'],
-            'description':form_data['description'],
-            'icon':form_data['icon'],
-            'schema': dummySchema
+            'id': db_worker.getApplicationsDictionary().__len__(),
+            'name': form_data['name'],
+            'description': form_data['description'],
+            'icon': form_data['icon'],
+            'schema': dummySchema if 'fibonacci' in form_data else (schemaMoreDummyThanDummy if 'nothing' in form_data else otherSchema ),
+            'computationStepsPackage': computation_step_package_fibonacci if 'fibonacci' in form_data else
+                                        (computation_step_package_cyberpunk if 'nothing' in form_data else computation_step_package_default )
         }
+        print("app created")
         db_worker.saveApplicationDictionary(app)
+        print("post ended")
         return "OK", 200
     return "Not implemented", 501
 
@@ -48,6 +138,9 @@ def application_add_form():
     if request.method == 'GET':
         return render_template('app_form.html')
 
-@app.route('/library/launcher/applications/<int:id>')
-def get_application(id):
-    return "OK", 200
+@app.route('/library/launcher/application/<string:id>')
+def get_stp_for_application(id):
+    csp = db_worker.getApplicationCSP(id)
+    return csp, 200
+
+
